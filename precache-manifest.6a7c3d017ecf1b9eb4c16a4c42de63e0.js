bbaf31658210b9eefb14ccbc5006529d52d37924
Beatricce <beatriz.de.oli.s@gmail.com>
2020-07-03 18:38:19 -0300

    Updates

---
+++ precache-manifest.6a7c3d017ecf1b9eb4c16a4c42de63e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194d470e3dbe325343b79db07534759a",
+    "url": "/ghreal2u/index.html"
+  },
+  {
+    "revision": "6cc498f13632f0cbb81f",
+    "url": "/ghreal2u/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "ea4de0a3fe138fa864a7",
+    "url": "/ghreal2u/static/css/main.f73826a7.chunk.css"
+  },
+  {
+    "revision": "6cc498f13632f0cbb81f",
+    "url": "/ghreal2u/static/js/2.a0a2332a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ghreal2u/static/js/2.a0a2332a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4de0a3fe138fa864a7",
+    "url": "/ghreal2u/static/js/main.0f1317e0.chunk.js"
+  },
+  {
+    "revision": "1be84c7066110a2eec98",
+    "url": "/ghreal2u/static/js/runtime-main.731963c3.js"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/ghreal2u/static/media/ghlogo.0ba2aa20.png"
+  }
+]);
